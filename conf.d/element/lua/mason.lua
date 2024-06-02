9049febd694ba067d568d410c9a957c30ba9b360
leoatchina <leoatchina@outlook.com>
2024-06-02 22:35:55 +0800

    update mason.lua

--- conf.d/element/lua/mason.lua
+++ conf.d/element/lua/mason.lua
@@ -1,5 +1,15 @@
-vim.keymap.set("n", "<M-l>m", [[<Cmd>Mason<Cr>]], { noremap = true, silent = true })
+if Installed('nvim-java') then
+  local registries = {
+    "github:nvim-java/mason-registry",
+    "github:mason-org/mason-registry"
+  }
+else
+  local registries = {
+    "github:mason-org/mason-registry"
+  }
+end
 require("mason").setup({
+  registries = registries,
   install_root_dir = vim.fn.expand("~/.leovim.d/mason"),
   ui = {
     icons = {
@@ -9,3 +19,4 @@ require("mason").setup({
     },
   },
 })
+vim.keymap.set("n", "<M-l>m", [[<Cmd>Mason<Cr>]], { noremap = true, silent = true })
