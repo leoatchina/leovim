1c717bc05f2cfcb5110e9f7a2a7cbc415a28d0db
leoatchina <leoatchina@outlook.com>
2025-04-13 16:32:41 +0800

    update

--- scripts/v.sh
+++ scripts/v.sh
@@ -1,1 +1,1 @@
-vim -u ~/.leovim/conf.d/init.vim --cmd "let g:preset_group=['apm']" "$@"
+vim -u ~/.leovim/conf.d/init.vim --cmd "let g:preset_group=['mcm']" "$@"
--- scripts/vi.sh
+++ scripts/vi.sh
@@ -1,1 +1,1 @@
-vim -u ~/.leovim/conf.d/init.vim --cmd "let g:preset_group=['mcm']" "$@"
+vim -u ~/.leovim/conf.d/init.vim --cmd "let g:preset_group=['coc']" "$@"
