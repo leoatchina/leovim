a30c42b580e3d361d807cbf12f069392a9b55d56
leoatchina <leoatchina@outlook.com>
2025-05-28 01:57:34 -0700

    update v.sh vi.sh

--- scripts/v.sh
+++ scripts/v.sh
@@ -2,10 +2,14 @@ if  [ -f "$HOME/.local/bin/vim.appimage" ] && [ -x "$HOME/.local/bin/vim.appimag
   VIMCMD="$HOME/.local/bin/vim.appimage"
 elif [ -f "$HOME/.local/vim9/bin/vim" ] && [ -x "$HOME/.local/vim9/bin/vim" ]; then
   VIMCMD="$HOME/.local/vim9/bin/vim"
-elif [ -x "vim" ]; then
-  VIMCMD="vim"
 else
-  echo "vim not executable"
-  exit 1
+  ret='0'
+  command -v vim >/dev/null 2>&1 || { local ret='1'; }
+  if [ "$ret" -ne 0 ]; then
+    echo "vim not executable"
+    exit 1
+  else
+    VIMCMD="vim"
+  fi
 fi
 $VIMCMD -u ~/.leovim/conf.d/init.vim --cmd "let g:preset_group=['mcm']" "$@"
